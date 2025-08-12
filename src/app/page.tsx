12e1caae7e603bcd8fa5d4736fc764995d5ae56a
Asrar Ahammad <asrarahammadshaik@gmail.com>
2025-08-12 22:27:43 +0530

    feat: add Prisma setup and initial schema
    - Added Prisma client and schema for User and Post models.
    - Updated package.json to include Prisma dependencies.
    - Refactored the main page component to simplify the structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
-export default function Home() {
-  return (
+const Page =  () =>{
+  return(
     <>
-    <div className="text-3xl font-extrabold text-rose-400">hello world</div>
-    <Button variant="new">
-      Click
-    </Button>
+    <div className="">
+    Hello world
+    </div>
     </>
-  );
+  )
 }
+
+export default Page;
